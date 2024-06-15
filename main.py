78405cf755577615d0c60fbb0c7d1d54433eb89e
sarakh9 <sadat.kh9@gmail.com>
2024-06-15 19:16:50 +0330

    tested key token function!

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 # Simple Math Interpreter
 # This is my first project that i am practicing typing and git at the same time.
 # plus i am learning how to create interpreter!
+
+from lexer import Lexer
+
+while True:
+    text = input("input: ")
+    lexer = Lexer(text)
+    token = lexer.generate_token()
+    print(list(token))
