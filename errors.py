d73bfcf65b40b9a97e9720c4c4ae03d20559d89b
sarakh9 <sadat.kh9@gmail.com>
2024-06-17 16:15:07 +0330

    added  error classes. first illigal char error. got tested!

---
+++ errors.py
@@ -0,0 +1,18 @@
+class Error:
+    def __init__(self, error_name, position, detail):
+        self.name_error = error_name
+        self.position = position
+        self.detail = detail
+    def as_string(self):
+        error = f"""  File '{self.position.file_name}', in line '{self.position.line}':\n\t'{self.position.line_context}'\n  {self.name_error}: {self.detail}"""
+        return error
+
+class IllegalCharacterError(Error):
+    def __init__(self ,position, detail):
+        super().__init__("EllegalCharacterError", position, detail)
+
+class Position:
+    def __init__(self, file_name, line, line_context):
+        self.file_name = file_name
+        self.line = line
+        self.line_context = line_context
