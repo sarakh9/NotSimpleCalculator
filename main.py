6b750b3908cdeb48c9b4b1a56860e9366bcf756e
sarakh9 <sadat.kh9@gmail.com>
2024-06-17 16:21:39 +0330

    lexer finished and tested! + main error handeling

--- main.py
+++ main.py
@@ -2,10 +2,18 @@
 # This is my first project that i am practicing typing and git at the same time.
 # plus i am learning how to create interpreter!
 
-from lexer import Lexer
+from lexer import Lexer, IllegalCharacterError
 
 while True:
-    text = input("input: ")
-    lexer = Lexer(text)
-    token = lexer.generate_token()
-    print(list(token))
+    try:
+        text = input("input: ")
+    except EOFError:
+        print("EOFError")
+        break
+    lexer = Lexer("testing.sk",text)
+    token, error = lexer.generate_token()
+    if error:
+        print(error.as_string())
+        break
+    else:
+        print(list(token))
