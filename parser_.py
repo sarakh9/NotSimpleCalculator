6b9d90ddb1c28f4228d67ec6b2ad8e497156675b
sarakh9 <sadat.kh9@gmail.com>
2024-06-17 20:28:12 +0330

    parsing errors added and tested

--- parser_.py
+++ parser_.py
@@ -1,6 +1,24 @@
 from tokens import TokenType
 from nodes import NumberNode, BinopNode
-from errors import Position
+from errors import Position, InvalidSyntaxError
+class ParseResult():
+    def __init__(self) -> None:
+        self.error = None
+        self.node = None
+    
+    def register(self, result):
+        if isinstance(result, ParseResult):
+            if result.error: self.error = result.error
+            return result.node
+
+    def succes(self, node):
+        self.node = node
+        return self
+
+    def fail(self, error):
+        self.error = error
+        return self
+
 
 class Parser:
     def __init__(self, file_name, tokens):
@@ -12,6 +30,7 @@ class Parser:
         self.advance()
         self.pos.line = 0
         self.token_index = 0
+    
     def advance(self):
         try:
             self.token_index = self.token_index +1
@@ -19,31 +38,45 @@ class Parser:
             return self.current_token
         except:
             return None
+    
     def parse(self):
         tree = self.expr()
+        if not tree.error and self.current_token.type != TokenType.EOF:
+            return tree.fail(InvalidSyntaxError(self.pos, "BAD!"))
         return tree
 
     def factor(self):
+        res = ParseResult()
         tok = self.current_token
         if tok.type == TokenType.NUMBER:
-            self.advance()
-            return NumberNode(tok)
+            res.register(self.advance())
+            return res.succes(NumberNode(tok))
+        else:
+            return res.fail(InvalidSyntaxError(self.pos, "expected int or float."))
+        
 
     def term(self):
-        left = self.factor()
+        res = ParseResult()
+        left = res.register(self.factor())
+        if res.error: 
+            return res
         while self.current_token.type == TokenType.BINOP and self.current_token.value == "*" or self.current_token.value == "/":
             op_tok = self.current_token
-            self.advance()
-            right = self.factor()
+            res.register(self.advance())
+            right = res.register(self.factor())
+            if res.error: return res
             left = BinopNode(op_tok, left, right)
-        return left
+        return res.succes(left)
 
     def expr(self):
-        left = self.term()
+        res = ParseResult()
+        left = res.register(self.term())
+        if res.error: return res
         while self.current_token.type == TokenType.BINOP and self.current_token.value == "+" or self.current_token.value == "-":
             op_tok = self.current_token
-            self.advance()
-            right = self.term()
+            res.register(self.advance())
+            right = res.register(self.term())
+            if res.error: return res
             left = BinopNode(op_tok, left, right)
-        return left
+        return res.succes(left)
 
